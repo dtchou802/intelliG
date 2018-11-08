fef91a940d878e7b67d60a6f8757f2db7eb17116
Daniel <dtchou802@gmail.com>
2018-11-07 19:24:24 -0800

    all reading move toward desired

--- Controller.java
+++ Controller.java
@@ -23,7 +23,10 @@ public class Controller extends Application {
 
 	static ArrayList<Integer> current = new ArrayList<>();
 	static ArrayList<Integer> desired = new ArrayList<>();
-	static IntegerProperty test = new SimpleIntegerProperty(75);
+	static IntegerProperty temp = new SimpleIntegerProperty(75);
+	static IntegerProperty humid = new SimpleIntegerProperty(45);
+	static IntegerProperty moist = new SimpleIntegerProperty(70);
+	static IntegerProperty co2 = new SimpleIntegerProperty(1480);
 
 
 	static SensorModule simulator = new SensorModule();
@@ -70,12 +73,18 @@ public class Controller extends Application {
 		current.set(4, simulator.generateCO2());
 
 
-		display.setLbl_temperature(test.get());
-		display.lbl_temperature.textProperty().bind(Bindings.concat("Temperature: ", test.asString()));
-		display.setLbl_humidity(current.get(1));
-		display.setLbl_moisture(current.get(2));
+		display.setLbl_temperature(temp.get());
+		display.lbl_temperature.textProperty().bind(Bindings.concat("Temperature: ", temp.asString()));
+		display.setLbl_humidity(humid.get());
+		display.lbl_humidity.textProperty().bind(Bindings.concat("Humidity: ", humid.asString(),"%"));
+		display.setLbl_moisture(moist.get());
+		display.lbl_moisture.textProperty().bind(Bindings.concat("Moisture: ", moist.asString(),"%"));
+
 		display.setLbl_PH(current.get(3));
-		display.setLbl_CO2(current.get(4));
+
+		display.setLbl_CO2(co2.get());
+		display.lbl_CO2.textProperty().bind(Bindings.concat("CO2: ", co2.asString(),"PPM"));
+
 
 		display.setLbl_desiredTemp(desired.get(0));
 		display.setLbl_desiredHumid(desired.get(1));
@@ -83,10 +92,11 @@ public class Controller extends Application {
 		display.setLbl_desiredPH(desired.get(3));
 		display.setLbl_desiredCO2(desired.get(4));
 
-		hardware.checkAir(test.getValue(), desired.get(0));
-		hardware.checkHumidity(current.get(1), desired.get(1));
-		hardware.checkMoisture(current.get(2), desired.get(2));
-		hardware.checkCO2(current.get(4), desired.get(4));
+
+		hardware.checkAir(temp.getValue(), desired.get(0));
+		hardware.checkHumidity(humid.get(), desired.get(1));
+		hardware.checkMoisture(moist.get(), desired.get(2));
+		hardware.checkCO2(co2.get(), desired.get(4));
 
 		display.setLbl_AirCond("A/C: " + hardware.getCond());
 		display.setLbl_Humidifier("Humidifier: " + ((hardware.isHumidifierOn() == true) ? "ON" : "OFF"));
@@ -102,13 +112,28 @@ public class Controller extends Application {
 							public void handle(ActionEvent actionEvent)
 							{
 								if(hardware.isAirOn()) {
-									test.set(test.getValue() - 1);//I think you should have used an Integer here.
-									hardware.checkAir(test.getValue(),desired.get(0));
+									temp.set(temp.getValue() - 1);//I think you should have used an Integer here.
+									hardware.checkAir(temp.getValue(),desired.get(0));
+								}
+								if(hardware.isHumidifierOn()) {
+									humid.set(humid.getValue() + 1);//I think you should have used an Integer here.
+									hardware.checkHumidity(humid.getValue(),desired.get(1));
+//									private boolean lightsOn;
+//									private boolean ventOn;
+//									private boolean CO2releaseOn;
+								}
+								if(hardware.isIrrigationOn()) {
+									moist.set(moist.getValue() + 1);//I think you should have used an Integer here.
+									hardware.checkMoisture(moist.getValue(),desired.get(2));
+								}
+								if(hardware.isCO2releaseOn()) {
+									co2.set(co2.getValue() + 1);//I think you should have used an Integer here.
+									hardware.checkMoisture(co2.getValue(),desired.get(4));
 								}
 							}
 						}
 				),
-				new KeyFrame(Duration.seconds(1))//Do something every second. In this case we are going to increment setStrP.
+				new KeyFrame(Duration.seconds(2))//Do something every second. In this case we are going to increment setStrP.
 		);
 		timeline.setCycleCount(1000);//Repeat this 10 times
 		timeline.play();
